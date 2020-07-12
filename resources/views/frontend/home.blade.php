81bbdc3d6d834c8c28fa69645e14e0927368d0fa
MRJAHID <mrjahid350@gmail.com>
2020-07-12 15:16:34 +0600

    homepage navbar

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="{{asset('frontend_assets/css/bootstrap.min.css')}}">
+    <link rel="icon" href="{{asset('frontend_assets/images/apple-touch-icon.png')}}">
+    <title>Park AnyWhere</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm bg-light navbar-light">
+        <a class="navbar-brand" href="#">
+            <img src="{{asset('frontend_assets/images/logo1.png')}}" alt="Logo" style="width:220px;">
+        </a>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">Rent out your driveway</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">Help</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" href="#">SignUp</a>
+            </li>
+        </ul>
+    </nav>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="{{asset('frontend_assets/js/jquery-3.5.1.min.js')}}"></script>
+    <script src="{{asset('frontend_assets/js/popper.min.js')}}"></script>
+    <script src="{{asset('frontend_assets/js/bootstrap.min.js')}}"></script>
+</body>
+</html>
